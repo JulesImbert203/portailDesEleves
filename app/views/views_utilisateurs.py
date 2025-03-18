df66e53d2d83f5a016b3d7ac649434296bf5baf2
Jules Imbert <jules.imbert@etu.minesparis.psl.eu>
2025-03-18 15:14:58 +0100

    modification des views de connexion et deconnexion

--- app/views/views_utilisateurs.py
+++ app/views/views_utilisateurs.py
@@ -42,44 +42,8 @@ def connexion():
 @login_required
 def deconnexion():
     logout_user()
-    return jsonify({'connecte':False}),200
+    if current_user.is_authenticated :
+        return jsonify({'connecte':True}),500
+    else :
+        return jsonify({'connecte':False}),200
 
-"""
-# Route pour afficher la page blanche de connexion (quand on arrive sur le portail sans etre connecte)
-@utilisateurs_bp.route('/page_blanche_de_connexion', methods=['GET'])
-def page_blanche_de_connexion():
-    return render_template('page_blanche_de_connexion.html') 
-"""
-
-
-
-"""
-# Route pour verifier les informations de connexion
-# est executee par le bouton du formulaire d'inscription
-@utilisateurs_bp.route('/connexion', methods=['POST'])
-def connexion():
-    if current_user.is_authenticated:
-        return redirect(url_for('index.accueil'))  # Si l'utilisateur est deja connecte, redirige vers l'accueil
-    username = request.form.get('username')
-    password = request.form.get('password')
-    # Recherche l'utilisateur par son nom d'utilisateur
-    utilisateur = Utilisateur.query.filter_by(nom_utilisateur=username).first()
-    # Verification du mot de passe
-    if utilisateur and check_password_hash(utilisateur.mot_de_passe, password):
-        login_user(utilisateur)  # Connecte l'utilisateur
-        if request.headers.get("X-Requested-With") == "XMLHttpRequest": # Verifie si la requete vient d'un script/API
-            return jsonify({"message": "Connexion réussie"}), 200
-        else :
-            return redirect(url_for('index.accueil'))  # Si c'est un navigateur, on redirige    
-    else:
-        if request.headers.get("X-Requested-With") == "XMLHttpRequest":
-            return jsonify({"message": "Nom d'utilisateur ou mot de passe incorrect"}), 401
-        else :
-            flash('Nom d\'utilisateur ou mot de passe incorrect.')  # Message d'erreur
-            return redirect(url_for('utilisateurs.page_blanche_de_connexion'))  # Redirige vers la page de connexion
-
-# se deconnecter    
-@utilisateurs_bp.route('/deconnexion', methods=['POST'])
-def deconnexion():
-    logout_user()  # Deconnecte l'utilisateur
-    return redirect(url_for('index.accueil'))"""
