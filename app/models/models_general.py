b75b0489d182bc978ebe4107e7b88a88703e567a
Jules Imbert <jules.imbert@etu.minesparis.psl.eu>
2025-03-19 17:07:52 +0100

    front sondages

--- app/models/models_general.py
+++ app/models/models_general.py
@@ -4,6 +4,7 @@ class GlobalVariable(db.Model):
     """
     Variables globales : id|"nom_variable"|"valeur_variable
     """
+    __tablename__ = 'global'
     id = db.Column(db.Integer, primary_key=True)
     key = db.Column(db.String(50), unique=True, nullable=False)
     value = db.Column(db.String(255), nullable=True)
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,21 @@
 export async function estAuthentifie() {
     const res = await fetch("http://localhost:5000/est_auth", { credentials: "include" });
     const data = await res.json();
-    return data.etat_connexion; // Flask renvoie { "etat_connexion": true/false , ...}
+    return data.etat_connexion; // Flask renvoie { "etat_connexion": true/false }
+  }
+
+export async function obtenirIdUser() {
+    const res = await fetch("http://localhost:5000/current_user_id", { credentials: "include" });
+    const data = await res.json();
+    return data.id_utilisateur; // Flask renvoie { "id_utilisateur": int ou None si on connecte, ...}
+  }
+
+export async function obtenirDataUser(id_utilisateur) {
+    const res = await fetch(`http://localhost:5000/api/users/obtenir_infos_profil/${id_utilisateur}`,
+      {credentials: "include"}
+    );
+    const data = await res.json();
+    return data; // au format JSON 
   }
   
 export async function seDeconnecter() {
@@ -24,4 +38,84 @@ export async function seConnecter(username, password) {
     const data = await res.json();
     return data.connecte; // Flask renvoie { "connecte": true/false }
   }
+  
+  export async function obtenirSondageDuJour() {
+    const res = await fetch(`http://localhost:5000/api/sondages/sondage_du_jour`,
+      {credentials: "include"}
+    );
+    const data = await res.json();
+    return data; // au format JSON 
+  }
+
+  
+  export async function requeteProposerSondage(question, reponses) {
+    const res = await fetch("http://localhost:5000/api/sondages/route_proposer_sondage", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include", // Si tu utilises des cookies de session
+      body: JSON.stringify({ question, reponses })
+    });
+  
+    const data = await res.json(); // Récupère la réponse JSON de l'API
+    return data ;
+  }
+  
+  export async function obtenirSondagesEnAttente() {
+    const res = await fetch(`http://localhost:5000/api/sondages/route_obtenir_sondages_en_attente`,
+      {credentials: "include"}
+    );
+    const data = await res.json();
+    return data; // au format JSON 
+  }
+
+  export async function validerSondage(id_sondage) {
+    await fetch(`http://localhost:5000/api/sondages/route_valider_sondage/${id_sondage}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include"
+    });
+  }
+
+  export async function supprimerSondage(id_sondage) {
+    await fetch(`http://localhost:5000/api/sondages/route_supprimer_sondage/${id_sondage}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include"
+    });
+  }
+
+  export async function sondageSuivant() {
+    try {
+      const response = await fetch(`http://localhost:5000/api/sondages/sondage_suivant`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include"
+      });
+  
+      if (!response.ok) { // Si la réponse n'est pas 200, c'est une erreur
+        const errorData = await response.json();
+        console.error("Erreur :", errorData.message);
+        // Afficher l'erreur dans l'interface utilisateur si nécessaire
+        alert(`Erreur : ${errorData.message}`);
+      } else {
+        const data = await response.json();
+        console.log("Sondage suivant :", data.message);  // Affiche "success" si ça fonctionne
+      }
+    } catch (error) {
+      console.error("Erreur réseau :", error);
+      // Afficher l'erreur dans l'interface utilisateur si nécessaire
+      alert(`Erreur réseau : ${error.message}`);
+    }
+  }
+  
+
+  export async function voterSondage(id_vote) {
+    await fetch(`http://localhost:5000/api/sondages/route_voter_sondage/${id_vote}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include"
+    });
+  }
+
+ 
   
