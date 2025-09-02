a132da600f6958ff40577ac28fee40f552b85722
Grégoire Girardet <gregoire.girardet@laposte.net>
2025-09-02 16:05:54 +0200

    Suppression views oblsolètes

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -8,6 +8,7 @@ from app.controllers.controllers_global import controllers_global
 from app.controllers.controllers_soifguard import controllers_soifguard
 from app.controllers.controllers_evenements import controllers_evenements
 from app.controllers.controllers_publications import controllers_publications
+from app.controllers.controllers_login import controllers_login
 
 # Creer un blueprint global qui regroupe tous les autres
 api = Blueprint('api', __name__)
@@ -20,5 +21,7 @@ api.register_blueprint(controllers_global, url_prefix='/global')
 api.register_blueprint(controllers_soifguard, url_prefix='/soifguard')
 api.register_blueprint(controllers_evenements, url_prefix='/evenements')
 api.register_blueprint(controllers_publications, url_prefix='/publications')
+api.register_blueprint(controllers_login, url_prefix='/login')
 
 # Ainsi, toutes les routes seront accessibles sous `/api/users` et `/api/sondages`, etc.
+from . import chat_socket
