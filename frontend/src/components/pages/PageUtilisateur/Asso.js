cd6335e9a0f8bfff62aa3968ca6eeb5b358ed18d
Grégoire Girardet <gregoire.girardet@laposte.net>
2025-09-06 19:53:35 +0200

    Liste plus jolie

--- frontend/src/components/pages/PageUtilisateur/Asso.js
+++ frontend/src/components/pages/PageUtilisateur/Asso.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 import '../../../assets/styles/liste_assos.css';
-import '../../../assets/styles/liste_assos.css';
+import '../../../assets/styles/asso.css';
 
 import { useLayout } from "../../../layouts/Layout";
 import { obtenirAssosUtilisateur } from "../../../api/api_utilisateurs";
@@ -28,24 +28,51 @@ export default function TabAsso({ id }) {
         setCurrentComponent(<Asso id={asso} />); // Change de composant
     };
 
-    return (
-        <div>
-            {assosActuelles.map((asso) => (
-                <div
-                    key={asso.id}
-                    className="liste-assos__grid-item"
-                    onClick={() => handleClick(asso.asso_id)}
-                >
-                    <img
-                        src={`http://${BASE_URL}/upload/associations/${asso.nom_dossier}/${asso.img}`}
-                        alt={asso.nom}
-                        className="liste-assos__image"
-                    />
-                    <p className="liste-assos__name">{asso.nom}</p>
-                    <p className="asso-membre-role">{asso.role}</p>
+    return (<>
+        <div className="liste-assos">
+            <h2>Assos actuelles</h2>
+            <div className="liste-assos__grid">
+                <div className="liste-assos__grid-container">
+                    {assosActuelles.map((asso) => (
+                        <div
+                            key={asso.id}
+                            className="liste-assos__grid-item"
+                            onClick={() => handleClick(asso.asso_id)}
+                        >
+                            <img
+                                src={`http://${BASE_URL}/upload/associations/${asso.nom_dossier}/${asso.img}`}
+                                alt={asso.nom}
+                                className="liste-assos__image"
+                            />
+                            <p className="liste-assos__name">{asso.nom}</p>
+                            <p className="asso-membre-role">{asso.role}</p>
+                        </div>
+                    ))}
                 </div>
-            ))}
+            </div>
         </div>
-    );
+        <div className="liste-assos">
+            <h2>Anciennes assos</h2>
+            <div className="liste-assos__grid">
+                <div className="liste-assos__grid-container">
+                    {assosAnciennes.map((asso) => (
+                        <div
+                            key={asso.id}
+                            className="liste-assos__grid-item"
+                            onClick={() => handleClick(asso.asso_id)}
+                        >
+                            <img
+                                src={`http://${BASE_URL}/upload/associations/${asso.nom_dossier}/${asso.img}`}
+                                alt={asso.nom}
+                                className="liste-assos__image"
+                            />
+                            <p className="liste-assos__name">{asso.nom}</p>
+                            <p className="asso-membre-role">{asso.role}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </>);
 }
 
